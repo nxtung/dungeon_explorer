3690fd2db4d1c1384d8dfad1feb9af07575fa06d
Tung <tungatinternet@gmail.com>
2023-11-15 14:45:04 +0700

    add files

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# dungeon_explorer
-View dungeon rooms details
+# exhib
+
+A demo dungeon exploration app.
+
+## Getting Started
+
+This project is a based on idea when I exploring facebook.
+
+A few resources to get you started if this is your first Flutter project:
+Depended packages:
+- carousel_slider: ^4.2.1
+
+Please refer to TransformationController, Matrix4, InteractiveViewer from Flutter docs for more details.
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:exhib/interactive.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Dungeon',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Dungeon App'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: InteractiveScreen(),
+    );
+  }
+}
